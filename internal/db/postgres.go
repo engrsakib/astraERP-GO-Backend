33d07acf687985413ab94a9d8c7cafd40a490bd9
engrsakib <snazmussakib01@gmail.com>
2026-02-05 13:28:56 +0600

    login and submit response full done and update successfully done. data read and dataset update

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -1,25 +1,28 @@
 package db
 
 import (
-	"log"
+    "log"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+    "gorm.io/gorm/logger"
 )
 
 func NewPostgres(dsn string) *gorm.DB {
-	
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		
-		PrepareStmt: false, 
-		
-		
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+    
+   
+    db, err := gorm.Open(postgres.New(postgres.Config{
+        DSN:                  dsn,
+        PreferSimpleProtocol: true, 
+    }), &gorm.Config{
+        
+        PrepareStmt: false,
+        
+        Logger: logger.Default.LogMode(logger.Info),
+    })
 
-	if err != nil {
-		log.Fatalf("failed to connect postgres: %v", err)
-	}
-	return db
+    if err != nil {
+        log.Fatalf("failed to connect postgres: %v", err)
+    }
+    return db
 }
--- internal/services/user/login_service.go
+++ internal/services/user/login_service.go
@@ -1,35 +1,47 @@
 package user
 
 import (
-    "errors"
+	"errors"
+	
 
-    dto "github.com/engrsakib/erp-system/internal/dto/user/login"
-    loginRepo "github.com/engrsakib/erp-system/internal/repository/login"
-    "github.com/engrsakib/erp-system/internal/utils"
+	dto "github.com/engrsakib/erp-system/internal/dto/user/login"
+	loginRepo "github.com/engrsakib/erp-system/internal/repository/login"
+	"github.com/engrsakib/erp-system/internal/utils"
 
-    "golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginService struct {
     Repo *loginRepo.LoginRepository
 }
 
+type LoginResponse struct {
+    User         map[string]interface{} `json:"user"`         
+    AccessToken  string                 `json:"accessToken"`  
+    RefreshToken string                 `json:"refreshToken"` 
+}
+
 func NewLoginService(repo *loginRepo.LoginRepository) *LoginService {
     return &LoginService{Repo: repo}
 }
 
-func (s *LoginService) Login(req dto.UserLoginRequest) (map[string]interface{}, error) {
+
+
+func (s *LoginService) Login(req dto.UserLoginRequest) (*LoginResponse, error) {
+
     user, err := s.Repo.FindByMobile(req.Mobile)
     if err != nil {
+       
         return nil, errors.New("invalid mobile or password")
     }
 
-    
+    // fmt.Println("REQ MOBILE:", req.Mobile)
+    // fmt.Println("DB USER MOBILE:", user.Mobile)
+
     if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)) != nil {
         return nil, errors.New("invalid mobile or password")
     }
 
-    // Token payload
     payload := map[string]interface{}{
         "id":     user.ID,
         "name":   user.Name,
@@ -37,26 +49,26 @@ func (s *LoginService) Login(req dto.UserLoginRequest) (map[string]interface{}, 
         "photo":  user.Photo,
     }
 
-    // Access Token
     accessToken, err := utils.GenerateToken(payload, "ACCESS_TOKEN_EXP")
     if err != nil {
         return nil, err
     }
 
-    // Refresh Token
+    
     refreshToken, err := utils.GenerateToken(payload, "REFRESH_TOKEN_EXP")
     if err != nil {
         return nil, err
     }
 
-    return map[string]interface{}{
-        "accessToken":  accessToken,
-        "refreshToken": refreshToken,
-        "user": map[string]interface{}{
+   
+    return &LoginResponse{
+        User: map[string]interface{}{
             "id":     user.ID,
             "name":   user.Name,
             "mobile": user.Mobile,
             "photo":  user.Photo,
         },
+        AccessToken:  accessToken,
+        RefreshToken: refreshToken,
     }, nil
 }
