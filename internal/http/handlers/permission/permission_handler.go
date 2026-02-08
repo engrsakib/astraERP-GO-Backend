78d80aa94c03e1e8ca9a28f7ff07b67048a68857
engrsakib <snazmussakib01@gmail.com>
2026-02-08 09:57:41 +0600

    update all data Swagger update ready

--- internal/http/handlers/permission/permission_handler.go
+++ internal/http/handlers/permission/permission_handler.go
@@ -18,6 +18,21 @@ func NewPermissionHandler(service *permissionService.PermissionService) *Permiss
 	return &PermissionHandler{Service: service}
 }
 
+
+// AssignPermissions godoc
+// @Summary      Assign Permissions
+// @Description  Super Admin can assign or update permissions for a specific user. This will replace old permissions.
+// @Tags         Admin
+// @Accept       json
+// @Produce      json
+// @Param        request body dto.AssignPermissionRequest true "User ID and List of Permission Slugs"
+// @Security     BearerAuth
+// @Success      200  {object}  map[string]interface{}  "Success Response"
+// @Failure      400  {object}  map[string]interface{}  "Invalid Request"
+// @Failure      401  {object}  map[string]interface{}  "Unauthorized"
+// @Failure      403  {object}  map[string]interface{}  "Forbidden (Only Super Admin)"
+// @Failure      500  {object}  map[string]interface{}  "Internal Server Error"
+// @Router       /api/v1/admin/assign-permission [post]
 // AssignPermissions handles the request to update user permissions
 func (h *PermissionHandler) AssignPermissions(c *gin.Context) {
 
