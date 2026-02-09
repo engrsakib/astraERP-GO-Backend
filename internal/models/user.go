7d156c7ece8830605db708bdc15624908c3c594c
engrsakib <snazmussakib01@gmail.com>
2026-02-09 11:52:13 +0600

    user model name index update and all data ready

--- internal/models/user.go
+++ internal/models/user.go
@@ -11,7 +11,7 @@ const (
 
 type User struct {
     ID       int64     `gorm:"primaryKey;autoIncrement" json:"id"`
-    Name     string    `gorm:"type:varchar(100);not null" json:"name"`
+    Name     string    `gorm:"type:varchar(100);index;not null" json:"name"`
     Mobile   string    `gorm:"type:varchar(20);uniqueIndex;not null" json:"mobile"`
     Email    string    `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
     Password string    `gorm:"type:varchar(255);not null" json:"-"`
